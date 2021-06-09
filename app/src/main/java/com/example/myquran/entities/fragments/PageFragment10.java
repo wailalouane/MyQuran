3e500677dc6f6a0a982ee4fc6b9ec1779b34c7bb
wail <wail.alouane@gmail.com>
2021-06-09 02:29:25 +0100

    complet the hide and show methode on all pages

--- app/src/main/java/com/example/myquran/entities/fragments/PageFragment10.java
+++ app/src/main/java/com/example/myquran/entities/fragments/PageFragment10.java
@@ -1,32 +1,167 @@
 package com.example.myquran.entities.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myquran.MyQuranApp;
 import com.example.myquran.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PageFragment10 extends Fragment {
-    String json;
+    String json;int cptShow=2;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Bundle bundle = getArguments();
         json = bundle.getString("json");
         String pagetitre = bundle.getString("titre");
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.page_10, container, false);
-        TextView pageTitle = rootView.findViewById(R.id.surahName);
-        TextView surhText = rootView.findViewById(R.id.surahText);
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.page_10, container, false);
+        TextView pageTitle=rootView.findViewById(R.id.surahName);
+        final TextView surhText = rootView.findViewById(R.id.surahText);
         surhText.setText(json);
         pageTitle.setText(pagetitre);
+
+        ImageButton hideBtn=rootView.findViewById(R.id.hideBtn);//button li nkhebbi bih
+        ImageButton showBtn=rootView.findViewById(R.id.showBtn);//button li n affichi bih
+
+
+        final List<String> subStringList=new ArrayList<>();
+        final ForegroundColorSpan fcsWhite=new ForegroundColorSpan(Color.WHITE);
+        final ForegroundColorSpan fcsBlack=new ForegroundColorSpan(Color.BLACK);
+
+        subStringList.add("(11)");
+        subStringList.add("(12)");
+        subStringList.add("(13)");
+        subStringList.add("(14)");
+        subStringList.add("(15)");
+        subStringList.add("(16)");
+        subStringList.add("(17)");
+        subStringList.add("(18)");
+        subStringList.add("(19)");
+        subStringList.add("(20)");
+        subStringList.add("(21)");
+        subStringList.add("(22)");
+        subStringList.add("(23)");
+        subStringList.add("(24)");
+        subStringList.add("(25)");
+        subStringList.add("(26)");
+        subStringList.add("(27)");
+        subStringList.add("(28)");
+
+
+
+
+
+
+
+
+        final String fullText=surhText.getText().toString();//all the page text
+
+        hideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//when we click on hide brn
+                SpannableString fullSpanneble=new SpannableString(fullText);//SpannableString pour pouvez changez la couleur
+                /*String subString="سورة الملك";*/
+                /*Toast.makeText(rootView.getContext(),"cliicked",Toast.LENGTH_SHORT).show();*/
+
+                fullSpanneble.setSpan(fcsWhite,0,fullSpanneble.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);//hada bach nkhebi kolch w naffichi ghir wach lazem
+
+                for (int i = 0; i <subStringList.size() ; i++) {
+                    ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                    int startIndex =fullText.indexOf(subStringList.get(i));
+                    fullSpanneble.setSpan(fcsblack,startIndex,startIndex+subStringList.get(i).length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+
+
+
+
+
+
+                cptShow=0;
+                surhText.setText(fullSpanneble);
+
+                ((MyQuranApp) getActivity().getApplication()).setSurahText(surhText);
+
+            }
+        });
+
+
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//show btn
+                String fullText=surhText.getText().toString();
+                SpannableString fullSpanneble=new SpannableString(fullText);
+                //   cptShow=2;
+                fullSpanneble.setSpan(fcsWhite,0,fullSpanneble.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+                switch (cptShow){
+
+                    case 0:
+                        cptShow++;
+                        for (int i = 0; i <subStringList.size()-1 ; i++) {
+                            ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                            int startIndex =fullText.indexOf(subStringList.get(i));
+                            fullSpanneble.setSpan(fcsblack,startIndex,startIndex+subStringList.get(i).length()+10,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        }
+                        ForegroundColorSpan fcsblack1=new ForegroundColorSpan(Color.BLACK);
+                        int startIndex1 =fullText.indexOf(subStringList.get(subStringList.size()-1));//nafiichi numero laakher
+                        fullSpanneble.setSpan(fcsblack1,startIndex1,startIndex1+subStringList.get(subStringList.size()-1).length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        break;
+                    case 1:
+                        cptShow++;
+                        for (int i = 0; i <subStringList.size()-1 ; i++) {
+                            ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                            int startIndex =fullText.indexOf(subStringList.get(i));
+                            fullSpanneble.setSpan(fcsblack,startIndex-10,startIndex+subStringList.get(i).length()+15, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        }
+                        ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                        ForegroundColorSpan fcsblack2=new ForegroundColorSpan(Color.BLACK);
+                        fullSpanneble.setSpan(fcsblack2,0,15,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        int startIndex =fullText.indexOf(subStringList.get(subStringList.size()-1));
+                        fullSpanneble.setSpan(fcsblack,startIndex-10,startIndex+subStringList.get(subStringList.size()-1).length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        break;
+                    case 2:
+                        fullSpanneble.setSpan(fcsBlack,0,fullSpanneble.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        Toast.makeText(rootView.getContext(),String.valueOf(cptShow),Toast.LENGTH_SHORT).show();
+
+                        //     cptShow=0;
+
+                }
+
+
+
+                surhText.setText(fullSpanneble);
+
+
+            }
+        });
+
+
+
+
 
 
         return rootView;
     }
+
 }
--- app/src/main/java/com/example/myquran/entities/fragments/PageFragment7.java
+++ app/src/main/java/com/example/myquran/entities/fragments/PageFragment7.java
@@ -1,32 +1,174 @@
 package com.example.myquran.entities.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myquran.MyQuranApp;
 import com.example.myquran.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PageFragment7 extends Fragment {
     String json;
+    int cptShow=2;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Bundle bundle = getArguments();
         json = bundle.getString("json");
         String pagetitre = bundle.getString("titre");
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.page_7, container, false);
-        TextView pageTitle = rootView.findViewById(R.id.surahName);
-        TextView surhText = rootView.findViewById(R.id.surahText);
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.page_7, container, false);
+        TextView pageTitle=rootView.findViewById(R.id.surahName);
+        final TextView surhText = rootView.findViewById(R.id.surahText);
         surhText.setText(json);
         pageTitle.setText(pagetitre);
+
+        ImageButton hideBtn=rootView.findViewById(R.id.hideBtn);//button li nkhebbi bih
+        ImageButton showBtn=rootView.findViewById(R.id.showBtn);//button li n affichi bih
+
+
+        final List<String> subStringList=new ArrayList<>();
+        final ForegroundColorSpan fcsWhite=new ForegroundColorSpan(Color.WHITE);
+        final ForegroundColorSpan fcsBlack=new ForegroundColorSpan(Color.BLACK);
+        subStringList.add("(35)");
+        subStringList.add("(36)");
+        subStringList.add("(37)");
+        subStringList.add("(38)");
+        subStringList.add("(39)");
+        subStringList.add("(40)");
+        subStringList.add("(41)");
+        subStringList.add("(42)");
+        subStringList.add("(43)");
+        subStringList.add("(44)");
+        subStringList.add("(45)");
+        subStringList.add("(46)");
+        subStringList.add("(47)");
+        subStringList.add("(48)");
+        subStringList.add("(49)");
+        subStringList.add("(50)");
+        subStringList.add("(51)");
+        subStringList.add("(52)");
+
+        subStringList.add("سورة المعارج");
+        subStringList.add("بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ");
+        subStringList.add("(1)");
+        subStringList.add("(2)");
+        subStringList.add("(3)");
+        subStringList.add("(4)");
+        subStringList.add("(5)");
+        subStringList.add("(6)");
+        subStringList.add("(7)");
+        subStringList.add("(8)");
+        subStringList.add("(9)");
+        subStringList.add("(10)");
+
+
+
+        final String fullText=surhText.getText().toString();//all the page text
+
+        hideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//when we click on hide brn
+                SpannableString fullSpanneble=new SpannableString(fullText);//SpannableString pour pouvez changez la couleur
+                /*String subString="سورة الملك";*/
+                /*Toast.makeText(rootView.getContext(),"cliicked",Toast.LENGTH_SHORT).show();*/
+
+                fullSpanneble.setSpan(fcsWhite,0,fullSpanneble.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);//hada bach nkhebi kolch w naffichi ghir wach lazem
+
+                for (int i = 0; i <subStringList.size() ; i++) {
+                    ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                    int startIndex =fullText.indexOf(subStringList.get(i));
+                    fullSpanneble.setSpan(fcsblack,startIndex,startIndex+subStringList.get(i).length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+
+
+
+
+
+
+                cptShow=0;
+                surhText.setText(fullSpanneble);
+
+                ((MyQuranApp) getActivity().getApplication()).setSurahText(surhText);
+
+            }
+        });
+
+
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//show btn
+                String fullText=surhText.getText().toString();
+                SpannableString fullSpanneble=new SpannableString(fullText);
+                //   cptShow=2;
+                fullSpanneble.setSpan(fcsWhite,0,fullSpanneble.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                switch (cptShow){
+
+                    case 0:
+                        cptShow++;
+                        for (int i = 0; i <subStringList.size()-1 ; i++) {
+                            ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                            int startIndex =fullText.indexOf(subStringList.get(i));
+                            fullSpanneble.setSpan(fcsblack,startIndex,startIndex+subStringList.get(i).length()+10,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        }
+                        ForegroundColorSpan fcsblack1=new ForegroundColorSpan(Color.BLACK);
+                        int startIndex1 =fullText.indexOf(subStringList.get(subStringList.size()-1));//nafiichi numero laakher
+                        fullSpanneble.setSpan(fcsblack1,startIndex1,startIndex1+subStringList.get(subStringList.size()-1).length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        break;
+                    case 1:
+                        cptShow++;
+                        for (int i = 0; i <subStringList.size()-1 ; i++) {
+                            ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                            int startIndex =fullText.indexOf(subStringList.get(i));
+                            fullSpanneble.setSpan(fcsblack,startIndex-10,startIndex+subStringList.get(i).length()+15, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        }
+                        ForegroundColorSpan fcsblack=new ForegroundColorSpan(Color.BLACK);
+                        ForegroundColorSpan fcsblack2=new ForegroundColorSpan(Color.BLACK);
+                        fullSpanneble.setSpan(fcsblack2,0,15, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        int startIndex =fullText.indexOf(subStringList.get(subStringList.size()-1));
+                        fullSpanneble.setSpan(fcsblack,startIndex-10,startIndex+subStringList.get(subStringList.size()-1).length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        break;
+                    case 2:
+                        fullSpanneble.setSpan(fcsBlack,0,fullSpanneble.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        Toast.makeText(rootView.getContext(),String.valueOf(cptShow),Toast.LENGTH_SHORT).show();
+
+                        //     cptShow=0;
+
+                }
+
+
+
+
+
+                surhText.setText(fullSpanneble);
+
+
+            }
+        });
+
 
 
         return rootView;
     }
+
+
 }
