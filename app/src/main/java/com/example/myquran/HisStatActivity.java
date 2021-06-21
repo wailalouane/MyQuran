c7175352f0acc78b00d16303ffef632ee8a6387f
ibrahim ghozlane <ibrahimghozlane2000@gmail.com>
2021-06-21 21:48:48 +0100

    editing design

---
+++ app/src/main/java/com/example/myquran/HisStatActivity.java
@@ -0,0 +1,32 @@
+package com.example.myquran;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.myquran.Adapter.HistoriqueVpAdapter;
+import com.example.myquran.entities.fragments.HistoriqueFragment;
+import com.example.myquran.entities.fragments.PagesHistoriqueFragment;
+import com.example.myquran.entities.fragments.PlusLusFragment;
+import com.google.android.material.tabs.TabLayout;
+
+public class HisStatActivity extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_his_stat);
+        mTabLayout=findViewById(R.id.tabLayout);
+        mViewPager=findViewById(R.id.viewPager);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        HistoriqueVpAdapter historiqueVpAdapter =new HistoriqueVpAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        historiqueVpAdapter.addFragment(new PagesHistoriqueFragment(),"PAGE STATS");
+        historiqueVpAdapter.addFragment(new HistoriqueFragment(),"HISTORIQUE");
+        historiqueVpAdapter.addFragment(new PlusLusFragment(),"MOST READ");
+    }
+}
