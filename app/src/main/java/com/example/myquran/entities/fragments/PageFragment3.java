7fc82a45355647af211f528558261b0891e873d8
ibrahim ghozlane <ibrahimghozlane2000@gmail.com>
2021-06-22 15:50:00 +0100

    editing page 3

--- app/src/main/java/com/example/myquran/entities/fragments/PageFragment3.java
+++ app/src/main/java/com/example/myquran/entities/fragments/PageFragment3.java
@@ -65,8 +65,8 @@ public class PageFragment3 extends Fragment {
         surhText.setText(s1);
         pageTitle.setText(pagetitre);
 
-        ImageButton hideBtn=rootView.findViewById(R.id.hideBtn);//button li nkhebbi bih
-        ImageButton showBtn=rootView.findViewById(R.id.showBtn);//button li n affichi bih
+        TextView hideBtn=rootView.findViewById(R.id.hideBtn);//button li nkhebbi bih
+        TextView showBtn=rootView.findViewById(R.id.showBtn);//button li n affichi bih
 
 
         final List<String> subStringList=new ArrayList<>();
